b95bd0c50fba4eb3299753cedf905ae51e57c3f7
홍광택 <hhkktt418@naver.com>
2022-08-30 18:16:36 +0900

    0830프론트 추가_홍광택
    채팅, 컨택 작성

---
+++ frtest/js.js
@@ -0,0 +1,19 @@
+function openPop(){
+
+    var pX = Math.round((window.screen.width/2) - 200);
+    var pY = Math.round((window.screen.height/2) - 350);
+
+    var windowsize = "width=400px, height=600px, left=" + pX + ", top=" + pY; 
+    var popup = window.open('C:/Users/soldesk/C0830/profile.html', '팝업', 
+    windowsize);
+  }
+
+  function openPop2(){
+
+    var pX = Math.round((window.screen.width/2) - 200);
+    var pY = Math.round((window.screen.height/2) - 350);
+
+    var windowsize = "width=400px, height=600px, left=" + pX + ", top=" + pY; 
+    var popup = window.open('C:/Users/soldesk/C0830/chat.html', '팝업2', 
+    windowsize);
+  }
